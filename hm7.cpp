314dc032bcefa0c6b978212d684736aec1ccc1bb
ConstAndTime <nbfbirov@mail.ru>
2021-06-08 07:46:50 +0300

    Complite 1 and 2 quest

---
+++ hm7.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<ctime>
+//1
+#define PROVERKA(x,y) ((((x) >= (0)) && ((x) < (y))) ? ("YES") : ("NO"))
+
+//2
+#define MASVALUE ((*(*arr) + (2)) + (2))
+
+//3
+
+int main (int argc, const char** argv) {
+
+//1
+int x = 8;
+int y = 11;
+  std::cout << PROVERKA(x,y) << std::endl;
+  std::cout << std::endl;
+
+//2
+int n = rand() % 90;
+  int arr[5][5] = {n};
+  std::cout << MASVALUE << std::endl;
+std::cout << std::endl;
+
+//3
+
+
+
+
+
+  return 0;
+}
